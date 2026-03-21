c13ac428227c3127cce7ef30b0b0b2e728d565c0
captain-sadami <>
2026-03-22 08:47:38 +0900

    modified addTask to authentication confirmation with getAppUser  and  try-catch handling

--- app/api/addTask/route.ts
+++ app/api/addTask/route.ts
@@ -1,25 +1,44 @@
 import { createSupabaseServer } from "@/lib/supabaseServer";
+import { getAppUser } from "@/lib/getAppUser";
+import { NextResponse } from "next/server";
+
 
 export async function POST(req:Request){
-  const body = await req.json();
-  const { title, description, goal_id, identity_id  } = body;
-
-  const supabase = createSupabaseServer();
-
-  const { error } = await supabase
-    .from("tasks")
-    .insert({
-      title,
-      description,
-      goal_id,
-      is_completed:false,
-      user_id: identity_id
-    });
-
-    if (error) {
-      console.error(error);
-      return new Response("Error", { status: 500 });
+  try {
+    const user = await getAppUser();
+    if (!user){
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    return new Response("OK", {status: 200 });
+    const identifiedAppUserId = user.appUserId;
+    const body = await req.json();
+    const { title, description, goal_id } = body;
+
+    if (!title || !goal_id) {
+      return NextResponse.json({ error: "Title and goal_id are ignored" }, {status:400});
+    }
+
+
+    const supabase = createSupabaseServer();
+    const { error } = await supabase
+      .from("tasks")
+      .insert({
+        title,
+        description,
+        goal_id,
+        is_completed:false,
+        user_id: identifiedAppUserId
+      });
+
+    if (error) throw error;
+
+    return NextResponse.json({ ok: true });
+  
+  } catch(err: any) {
+      console.error("Add Task Error: ", err);
+      return NextResponse.json(
+        { error: err.message || "Internal Server Error" },
+        { status: 500 }
+      );
+  }
 }
