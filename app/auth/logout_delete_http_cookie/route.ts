7214ebaad077c972fcecc766ccf631e2e5f5d998
captain-sadami <>
2026-03-25 22:28:46 +0900

    modified the way to disable coikie

--- app/auth/logout_delete_http_cookie/route.ts
+++ app/auth/logout_delete_http_cookie/route.ts
@@ -1,12 +1,25 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+
 export async function POST() {
-  const cookieStore = await cookies();
+  const response = NextResponse.json({success: true});
 
   // kill cookies
-  cookieStore.set("access_token", "", {maxAge: 0, path: "/"});
-  cookieStore.set("id_token", "", {maxAge: 0, path: "/"});
+  response.cookies.set("access_token", "", {
+    path: "/",
+    maxAge: 0,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
 
-  return NextResponse.json({success: true});
+  response.cookies.set("id_token", "", {
+    path: "/",
+    maxAge: 0,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  })
+
+  return response;
 }
